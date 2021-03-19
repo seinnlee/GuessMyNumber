5b5f2d239180647a69a4ca3a50ddcb19a3fd2839
sein <44532313+seinnlee@users.noreply.github.com>
2021-03-19 21:21:50 +0900

    Created a basic guessing game

--- src/ui/GuessMyNumber.java
+++ src/ui/GuessMyNumber.java
@@ -1,4 +1,24 @@
 package ui;
 
+import user.User;
+
+import java.util.Random;
+import java.util.Scanner;
+
 public class GuessMyNumber {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter your name:");
+        Scanner userInput = new Scanner(System.in);
+        User user = new User (userInput.next());
+
+        System.out.println("Please select your mode:");
+        System.out.println(" 1. Easy Mode");
+        System.out.println(" 2. Medium Mode");
+        System.out.println(" 3. Hard Mode");
+
+        int userLevel = userInput.nextInt();
+        user.userLevel(userLevel);
+
+    }
 }
